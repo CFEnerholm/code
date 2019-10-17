1c833603c1eee0d58d5eb9936979f0412e22da90
Carl-Fredrik Enerholm <carl-fredrik@CarlFredriksMBP.lan>
2019-10-17 08:35:15 +0200

    nbv'

--- javaScript/sendMail.js
+++ javaScript/sendMail.js
@@ -1,24 +1,15 @@
 //93890fd2-bf02-457d-8945-564547ff9e0a 
 //https://smtpjs.com/
 
-function send(){
-
-  
-    alert("HEj");
-    
-  
-Email.send({
+{
+  Email.send({
 
    // SecureToken : "93890fd2-bf02-457d-8945-564547ff9e0a",
-   Host : "mail.nilssonenerholm.se",
+    Host : "smtp.nilssonenerholm.se",
     Username : "visitor@nilssonenerholm.se",
     Password : "ContactForm",
     To : "info@nilssonenerholm.se",
     From : "visitor@nilssonenerholm.se",
     Subject : "Från nilssonenerholm.se",
     Body :  "TEsT!!"
-    
-}).then(
-  message => alert(message)
-);
-};
+  }).then( message => alert(message));});
