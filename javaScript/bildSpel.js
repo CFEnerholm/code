9f485fe669f50c76429a79c753e0cdf1ef03262a
Carl-Fredrik Enerholm <carl-fredrik@CarlFredriksMBP.lan>
2019-10-21 16:08:09 +0200

    kgh

--- javaScript/bildSpel.js
+++ javaScript/bildSpel.js
@@ -7,7 +7,7 @@ function plusDivs(n) {
 
 function showDivs(n) {
   var i;
-  var x = document.getElementsByClassName("bildIBildspel");
+  var x = document.getElementsByClassName("bildspelsBild");
   if (n > x.length) {slideIndex = 1}
   if (n < 1) {slideIndex = x.length}
   for (i = 0; i < x.length; i++) {
