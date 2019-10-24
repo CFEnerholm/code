b6cf6dca577c08c0dafa188c8f1b8e66aa7e52d1
Mats Nilsson <mats@Matss-MacBook-Pro.local>
2019-10-24 23:25:48 +0200

    m

--- javaScript/mail.php
+++ javaScript/mail.php
@@ -1,32 +1,19 @@
-/**
-
-
-//if($_POST){
-    //$to = "info@nilssonenerholm.se"; // this is your Email address
-    //$from = $_POST['mail']; // this is the sender's Email address
-    //$first_name = $_POST['name'];
-    //$phone = $_POST['phone'];
-    //$subject = "Medelande från nilssonenerholm.se";
-    //$subject2 = "Kopia på det formulär du skicka på nilssonenerholm.se";
-    //$message = $first_name .  " wrote the following:" . "\n\n" . $_POST['message'];
-    //$message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
-
-    //$headers = "From:" . $from;
-    //$headers2 = "From:" . $to;
-    //mail($to,$subject,$message,$headers);
-    //mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-   
-    //header('Location: startsida.html/');
-    // You cannot use header and echo together. It's one or the other.
-  //  }
-
- 
 
 <?php
 if($_POST){
+    $to = "info@nilssonenerholm.se";
+    $subject = "Medelande från nilssonenerholm.se";
+    $subject2 = "Kopia på det formulär du skicka på nilssonenerholm.se";
     $name = $_POST['name'];
-    $email = $_POST['email'];
+    $from = $_POST['mail'];
     $phone = $_POST['phone'];
-    $message = $_POST['message'];
+    $message = $name .  " skrev:" . "\n\n" . $_POST['message'] . "\n\n"  . $_POST['name'] . "\n\n" . $_POST['mail'] . "\n\n" . $_POST['phone'];
+    $message2 = "Här är en kopia på ditt meddelande " . $first_name . "\n\n" . $_POST['message'];
 
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+
+    mail($to,$subject,$message,$headers); //send email
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+}
 ?>
