544d049ce7455b244acfb82b5ed62398ad4bcf58
Mats Nilsson <imacserver@server.nilsson.red>
2019-11-20 08:51:24 +0100

    m

---
+++ Segelstads/javascript/mail.js
@@ -0,0 +1,34 @@
+function sendMail(){
+    var isValid = document.querySelector('form').reportValidity();
+    if(!$('#lastName').val()){
+    
+   
+    if (!isValid  ) {
+        return false;
+    }
+    var data = {
+        name: $("#name").val(),
+        mail: $("#mail").val(),
+        phone: $("#phone").val(),
+        message: $("#message").val()
+    };
+    $.ajax({
+        type: "POST",
+        url: "javascript/mail.php",
+        data: data,
+        success: function(){
+            
+        }
+    });
+    showModal('kontaktModal');
+    $("#formMail").trigger('reset');
+    return false;
+}
+}
+
+
+
+function showModal(Modal){
+    var modal = document.getElementById(Modal);
+    modal.style.display = "block";
+}
