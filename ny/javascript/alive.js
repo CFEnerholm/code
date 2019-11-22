ab1191353e1feb8057bebbad372de3c067ade818
Carl-Fredrik Enerholm <carl-fredrik@CarlFredriksMBP.lan>
2019-11-22 10:13:35 +0100

    ljkgh

--- ny/javascript/alive.js
+++ ny/javascript/alive.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
-    $("h1").delay(100).animate({"opacity":"1"}, 1000);
-    $("h3").delay(300).animate({"opacity":"1"}, 2000);
+    $("h2").css({"left":"2000px"}).animate({"left":"0px"}, "slow");
+    $("h6").css({"left":"-2000px"}).animate({"left":"0px"}, "slow");
+
+    $(".project1").css({"right":"2000px"}).stop().delay(500).animate({"right":"0px"}, "slow");
+    $(".project2").css({"top":"2000px"}).stop().delay(1000).animate({"top":"0px"}, "slow");
+    $(".project3").css({"left":"2000px"}).stop().delay(1500).animate({"left":"0px"}, "slow");
+
   });
+
+  $('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+    return false;
+})
