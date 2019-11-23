643a5c42ed4c9e58aedcdc1475c8724886c7055e
Carl-Fredrik Enerholm <carl-fredrik@CarlFredriksMBP.lan>
2019-11-23 10:48:17 +0100

    kjgh

--- ny/javascript/alive.js
+++ ny/javascript/alive.js
@@ -1,16 +1,16 @@
-$(document).ready(function(){
-    $("h2").css({"left":"2000px"}).animate({"left":"0px"}, "slow");
-    $("h6").css({"left":"-2000px"}).animate({"left":"0px"}, "slow");
+// $(document).ready(function(){
+//     $("h2").css({"left":"2000px"}).animate({"left":"0px"}, "slow");
+//     $("h6").css({"left":"-2000px"}).animate({"left":"0px"}, "slow");
 
-    $(".project1").css({"right":"2000px"}).stop().delay(500).animate({"right":"0px"}, "slow");
-    $(".project2").css({"top":"2000px"}).stop().delay(1000).animate({"top":"0px"}, "slow");
-    $(".project3").css({"left":"2000px"}).stop().delay(1500).animate({"left":"0px"}, "slow");
+//     $(".project1").css({"right":"2000px"}).stop().delay(500).animate({"right":"0px"}, "slow");
+//     $(".project2").css({"top":"2000px"}).stop().delay(1000).animate({"top":"0px"}, "slow");
+//     $(".project3").css({"left":"2000px"}).stop().delay(1500).animate({"left":"0px"}, "slow");
 
-  });
+//   });
 
-  $('a').click(function(){
-    $('html, body').animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top
-    }, 500);
-    return false;
-})
+//   $('a').click(function(){
+//     $('html, body').animate({
+//         scrollTop: $( $.attr(this, 'href') ).offset().top
+//     }, 500);
+//     return false;
+// })
--- ny/javascript/fade-in.js
+++ ny/javascript/fade-in.js
@@ -1,10 +1,17 @@
+$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+    return false;
+})
+
 $(document).ready(function() {
     
     /* Every time the window is scrolled ... */
     $(window).scroll( function(){
     
         /* Check the location of each desired element */
-        $('.hiddenTest').each( function(i){
+        $('.fadeIn').each( function(i){
             
             var bottom_of_object = $(this).offset().top + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
@@ -12,12 +19,44 @@ $(document).ready(function() {
             /* If the object is completely visible in the window, fade it it */
             if( bottom_of_window > bottom_of_object ){
                 
-                $(this).animate({'opacity':'1'},500);
+                $(this).animate({'opacity':'1'},1000);
                     
             }
             
         }); 
-    
+
+        $('.fadeInDelay500').each( function(i){          
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            if( bottom_of_window > bottom_of_object ){
+                $(this).delay(500).animate({'opacity':'1'},1000); 
+            }        
+        }); 
+
+        $('.fadeInDelay1000').each( function(i){
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            if( bottom_of_window > bottom_of_object ){         
+                $(this).delay(1000).animate({'opacity':'1'},1000); 
+            }         
+        }); 
+
+        $('.swipeInFromLeft').each( function(i){
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            if( bottom_of_window > bottom_of_object ){         
+                $(this).animate({"left":"0px"}, "slow");
+            }         
+        }); 
+
+        $('.swipeInFromRight').each( function(i){
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            if( bottom_of_window > bottom_of_object ){         
+                $(this).animate({"left":"0px"}, "slow");
+            }         
+        }); 
+
     });
     
 });
