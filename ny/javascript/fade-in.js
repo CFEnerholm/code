93732e555edfce594e13e8e128623de8b38f50c1
Carl-Fredrik Enerholm <carl-fredrik@CarlFredriksMBP.lan>
2019-11-24 09:26:36 +0100

    kjg

--- ny/javascript/fade-in.js
+++ ny/javascript/fade-in.js
@@ -45,6 +45,14 @@ function Go(){
                     $(this).delay(1000).animate({'opacity':'1'},1000); 
                 }         
             }); 
+
+            $('.fadeInDelay1500').each( function(i){
+                var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+                var bottom_of_window = $(window).scrollTop() + $(window).height();
+                if( bottom_of_window > bottom_of_object ){         
+                    $(this).delay(1500).animate({'opacity':'1'},1000); 
+                }         
+            }); 
     
             $('.swipeInFromLeft').each( function(i){
                 var bottom_of_object = $(this).offset().top + $(this).outerHeight();
@@ -59,6 +67,30 @@ function Go(){
                 var bottom_of_window = $(window).scrollTop() + $(window).height();
                 if( bottom_of_window > bottom_of_object ){         
                     $(this).animate({"left":"0px"}, "slow");
+                }         
+            }); 
+
+            $('.swipeInFromRightDelay500').each( function(i){
+                var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+                var bottom_of_window = $(window).scrollTop() + $(window).height();
+                if( bottom_of_window > bottom_of_object ){         
+                    $(this).delay(500).animate({"left":"0px"}, "slow");
+                }         
+            }); 
+
+            $('.swipeInFromRightDelay1000').each( function(i){
+                var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+                var bottom_of_window = $(window).scrollTop() + $(window).height();
+                if( bottom_of_window > bottom_of_object ){         
+                    $(this).delay(1000).animate({"left":"0px"}, "slow");
+                }         
+            }); 
+
+            $('.swipeInFromRightDelay1500').each( function(i){
+                var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+                var bottom_of_window = $(window).scrollTop() + $(window).height();
+                if( bottom_of_window > bottom_of_object ){         
+                    $(this).delay(1500).animate({"left":"0px"}, "slow");
                 }         
             }); 
 }
