1c3e2c7eabb7e01efb32e5b5e44f75b6d4bbc195
Carl-Fredrik Enerholm <carl-fredrik@CarlFredriksMBP.lan>
2019-11-25 15:40:11 +0100

    ,jhkgfbc

--- ny/javascript/fade-in.js
+++ ny/javascript/fade-in.js
@@ -12,6 +12,26 @@ $(document).ready(function () {
     $(window).scroll(function () {
         Go();
     });
+
+    $('.header2').each(function (i) {
+        var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+        if (bottom_of_window > bottom_of_object) {
+
+            $(this).delay(900).animate({ 'opacity': '0.9' }, 1500);
+
+        }
+    });
+
+    $('.header').each(function (i) {
+        var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+        if (bottom_of_window > bottom_of_object) {
+
+            $(this).delay(2500).animate({ 'opacity': '0.9' }, 1500);
+
+        }
+    });
 });
 
 
