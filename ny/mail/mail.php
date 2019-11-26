f6c079822f58fc7db56e34adaebafd932350420d
Mats Nilsson <imacserver@server.nilsson.red>
2019-11-26 13:33:29 +0100

    m

--- ny/mail/mail.php
+++ ny/mail/mail.php
@@ -18,13 +18,21 @@ $mail->AddReplyTo($_POST["mail"], $_POST["name"]);
 $mail->AddAddress("info@newsab.se");	
 $mail->Subject = "Mail från hemsidan";
 $mail->WordWrap   = 80;
-$mail->MsgHTML($_POST["message"]);
+//$mail->MsgHTML($_POST["message"]);
+
+$mail->Body = <<<EOT
+Email: {$_POST['mail']}
+Namn: {$_POST['name']}
+Telefon: {$_POST['phone']} 
+Meddelande: 
+{$_POST['message']}
+EOT;
 
 if(is_array($_FILES)) {
 	$mail->AddAttachment($_FILES['file']['tmp_name'],$_FILES['file']['name']); 
 }
 
-$mail->IsHTML(true);
+$mail->IsHTML(false);
 if(!$mail->Send()) {
 	echo "Problem in Sending Mail.";
 } else {
